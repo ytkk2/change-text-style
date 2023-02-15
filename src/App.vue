4a95d94cd1278d3c9a5aa325e4ece17109e1dc68
ytkk2 <yutakakatojp@gmail.com>
2023-02-15 14:38:52 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div :class="{ textVertical: isActive }">
+  <textarea class="textArea"></textarea>
+</div> 
+  <button @click="swichToVertical">change</button>
+ 
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isActive = ref(false)
+const swichToVertical = () => {
+ isActive.value = !isActive.value
+}
+</script>
+
+<style>
+.textVertical {
+  writing-mode:  vertical-rl;
+}
+</style>
